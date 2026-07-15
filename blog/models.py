084576f32c08a0d02425e4e213a9367cb94f6b61
Sunny-Creature <soniaisissoninha119@gmail.com>
2026-07-15 09:55:22 -0300

    html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Prato(models.Model):
+    nome_prato = models.CharField(max_length=100)
+    categoria_prato = models.CharField(max_length=50)
+    imagem_prato = models.ImageField(upload_to="imagens_pratos/")
